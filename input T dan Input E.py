3ac36ef0cae8bf1c852a264e3260eabd6aa0dea8
riskanadela <127011376+riskanadela@users.noreply.github.com>
2023-06-12 17:04:38 +0700

    Update input T dan Input E.py

--- input T dan Input E.py
+++ input T dan Input E.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import os
+import calendar
 
 def hitung_siklus_mens():
     rata_waktu_mens = int(input("Masukkan rata-rata durasi menstruasi (dalam hari): "))
@@ -16,7 +17,7 @@ def hitung_siklus_mens():
         tgl_akhir_folikular_berikutnya = tgl_awal_folikular_berikutnya + timedelta(days=9)
 
     tgl_awal_ovulasi_berikutnya = tgl_akhir_folikular_berikutnya + timedelta(days=1)
-    tgl_akhir_ovulasi_berikutnya = tgl_akhir_folikular_berikutnya + timedelta(days=3)
+    tgl_akhir_ovulasi_berikutnya = tgl_akhir_folikular_berikutnya + timedelta(days=4)
 
     tgl_awal_luteal_berikutnya = tgl_akhir_ovulasi_berikutnya + timedelta(days=1)
     if rata_waktu_mens > 5:
@@ -28,7 +29,6 @@ def hitung_siklus_mens():
     durasi_folikular = (tgl_akhir_folikular_berikutnya - tgl_awal_folikular_berikutnya).days
     durasi_ovulasi = (tgl_akhir_ovulasi_berikutnya - tgl_awal_ovulasi_berikutnya).days
     durasi_luteal = (tgl_akhir_luteal_berikutnya - tgl_awal_luteal_berikutnya).days
-
     return (
         tgl_awal_mens_berikutnya.date(),
         tgl_akhir_mens_berikutnya.date(),
@@ -47,9 +47,9 @@ def hitung_siklus_mens():
 hasil = hitung_siklus_mens()
 
 def simpan_ke_csv_1(hasil, nama_file):
-    header = '    tgl awal mens,              tgl akhir mens,             tgl awal folik,             tgl akhir folik,                tgl awal ovul,              tgl akhir ovul,             tgl awal lut,           tgl akhir lut'
+    header = '    tgl awal mens,              tgl akhir mens,             tgl awal folik,             tgl akhir folik,                tgl awal ovul,              tgl akhir ovul,             tgl awal lut,           tgl akhir lut,'
     template_csv = '''    
-      {}                   {}                  {}                  {}                     {}                  {}                 {}              {}          '''.format(
+      {},                  {},                 {},                 {},                    {},                 {},                 {},             {},          '''.format(
             hasil[0].strftime('%d-%m-%Y'),
             hasil[1].strftime('%d-%m-%Y'),
             hasil[2].strftime('%d-%m-%Y'),
@@ -68,17 +68,18 @@ def simpan_ke_csv_1(hasil, nama_file):
 simpan_ke_csv_1(hasil, 'siklus_mens.csv')
 
 def simpan_ke_csv_2(hasil, nama_file):
-    header = 'Durasi Mens,        Durasi Folikular,       Durasi Ovulasi,     Durasi Luteal'
+    header = '     Bulan,      Durasi Mens,      Durasi Folikular,     Durasi Ovulasi,   Durasi Luteal,'
     template_csv = ''' 
-        {}                     {}                     {}                   {}'''.format(
+      {},            {},                  {},                    {},              {},'''.format(
+        calendar.month_name[hasil[0].month], 
         hasil[8],
         hasil[9],
         hasil[10],
-        hasil[11]
+        hasil[11],
     )
     file_exists = os.path.isfile(nama_file)
     file_datamens = open(nama_file, 'a')
-    if not file_exists:  
+    if not file_exists:
         file_datamens.write(header)
     file_datamens.write(template_csv)
     file_datamens.close() 
