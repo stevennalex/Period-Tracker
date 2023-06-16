497bd939f0650c47cd6d7831f40a0b6c8fdbf7ef
stevennalex <126965379+stevennalex@users.noreply.github.com>
2023-06-17 03:30:41 +0700

    Update input_T_dan_Input_E.py

--- input_T_dan_Input_E.py
+++ input_T_dan_Input_E.py
@@ -1,34 +1,42 @@
 from datetime import timedelta, datetime
 import os
 import calendar
+import csv
+import pandas as pd
 
-def hitung_siklus_mens(tgl_terakhir_mens, rata_waktu_mens):
-    tgl_terakhir_mens = datetime.strptime(tgl_terakhir_mens, '%d-%m-%Y')
-    
-    tgl_awal_mens_berikutnya = tgl_terakhir_mens + timedelta(days=28)
-    tgl_akhir_mens_berikutnya = tgl_terakhir_mens + timedelta(days=28 + rata_waktu_mens)
+df = pd.read_csv('data_input.csv')
+
+tanggal = df['Tanggal'].astype(str)
+ratarata = int(df['rata-rata'].values[0])
+tanggal = pd.to_datetime(tanggal, format='%d-%m-%Y').iloc[0]
+
+def hitung_siklus_mens(tanggal, ratarata):
+        
+
+    tgl_awal_mens_berikutnya = tanggal + timedelta(days=28)
+    tgl_akhir_mens_berikutnya = tanggal + timedelta(days=27 + ratarata)
 
     tgl_awal_folikular_berikutnya = tgl_akhir_mens_berikutnya + timedelta(days=1)
-    if rata_waktu_mens > 5:
-        tgl_akhir_folikular_berikutnya = tgl_awal_folikular_berikutnya + timedelta(days=7)
+    if ratarata > 5:
+        tgl_akhir_folikular_berikutnya = tgl_awal_folikular_berikutnya + timedelta(days=6)
     else:
-        tgl_akhir_folikular_berikutnya = tgl_awal_folikular_berikutnya + timedelta(days=9)
+        tgl_akhir_folikular_berikutnya = tgl_awal_folikular_berikutnya + timedelta(days=8)
 
     tgl_awal_ovulasi_berikutnya = tgl_akhir_folikular_berikutnya + timedelta(days=1)
-    tgl_akhir_ovulasi_berikutnya = tgl_akhir_folikular_berikutnya + timedelta(days=4)
+    tgl_akhir_ovulasi_berikutnya = tgl_akhir_folikular_berikutnya + timedelta(days=3)
 
     tgl_awal_luteal_berikutnya = tgl_akhir_ovulasi_berikutnya + timedelta(days=1)
-    if rata_waktu_mens > 5:
-        tgl_akhir_luteal_berikutnya = tgl_awal_luteal_berikutnya + timedelta(days=9)
+    if ratarata > 5:
+        tgl_akhir_luteal_berikutnya = tgl_awal_luteal_berikutnya + timedelta(days=8)
     else:
-        tgl_akhir_luteal_berikutnya = tgl_awal_luteal_berikutnya + timedelta(days=11)
+        tgl_akhir_luteal_berikutnya = tgl_awal_luteal_berikutnya + timedelta(days=10)
 
-    durasi_mens = rata_waktu_mens
-    durasi_folikular = (tgl_akhir_folikular_berikutnya - tgl_awal_folikular_berikutnya).days
-    durasi_ovulasi = (tgl_akhir_ovulasi_berikutnya - tgl_awal_ovulasi_berikutnya).days
-    durasi_luteal = (tgl_akhir_luteal_berikutnya - tgl_awal_luteal_berikutnya).days
+    durasi_mens = ratarata
+    durasi_folikular = (tgl_akhir_folikular_berikutnya - tgl_awal_folikular_berikutnya  + timedelta(days=1)).days
+    durasi_ovulasi = (tgl_akhir_ovulasi_berikutnya - tgl_awal_ovulasi_berikutnya + timedelta(days=1)).days
+    durasi_luteal = (tgl_akhir_luteal_berikutnya - tgl_awal_luteal_berikutnya + timedelta(days=1)).days
 
-    return (
+    hasil = (
         tgl_awal_mens_berikutnya.date(),
         tgl_akhir_mens_berikutnya.date(),
         tgl_awal_folikular_berikutnya.date(),
@@ -41,15 +49,16 @@ def hitung_siklus_mens(tgl_terakhir_mens, rata_waktu_mens):
         durasi_folikular,
         durasi_ovulasi,
         durasi_luteal,
-    )
+        )
+    simpan_ke_csv_1(hasil, 'siklus_mens.csv')
+    simpan_ke_csv_2(hasil, 'durasi_siklus.csv')
 
-hitung_siklus_mens()
-hasil = hitung_siklus_mens()
+    return hasil
+
 
 def simpan_ke_csv_1(hasil, nama_file):
-    header = '    tgl awal mens,              tgl akhir mens,             tgl awal folik,             tgl akhir folik,                tgl awal ovul,              tgl akhir ovul,             tgl awal lut,           tgl akhir lut,'
-    template_csv = '''    
-      {},                  {},                 {},                 {},                    {},                 {},                 {},             {},          '''.format(
+    header = 'tgl awal mens,tgl akhir mens,tgl awal folik,tgl akhir folik,tgl awal ovul,tgl akhir ovul,tgl awal lut,tgl akhir lut'
+    template_csv = '\n{},{},{},{},{},{},{},{}'.format(
             hasil[0].strftime('%d-%m-%Y'),
             hasil[1].strftime('%d-%m-%Y'),
             hasil[2].strftime('%d-%m-%Y'),
@@ -64,13 +73,9 @@ def simpan_ke_csv_1(hasil, nama_file):
     file_datamens.write(template_csv)
     file_datamens.close()
 
-
-simpan_ke_csv_1(hasil, 'siklus_mens.csv')
-
 def simpan_ke_csv_2(hasil, nama_file):
-    header = '     Bulan,      Durasi Mens,      Durasi Folikular,     Durasi Ovulasi,   Durasi Luteal,'
-    template_csv = ''' 
-      {},            {},                  {},                    {},              {},'''.format(
+    header = 'Bulan,Durasi Mens,Durasi Folikular,Durasi Ovulasi,Durasi Luteal'
+    template_csv ='\n{},{},{},{},{}'.format(
         calendar.month_name[hasil[0].month], 
         hasil[8],
         hasil[9],
@@ -82,6 +87,6 @@ def simpan_ke_csv_2(hasil, nama_file):
     if not file_exists:
         file_datamens.write(header)
     file_datamens.write(template_csv)
-    file_datamens.close() 
+    file_datamens.close()
 
-simpan_ke_csv_2(hasil, 'durasi_siklus.csv')
+hasil = hitung_siklus_mens(tanggal, ratarata)
